7a8e060f6a0fac81edc5519cd2e7b2371cba09ad
seank42 <N00211599@iadt.ie>
2024-08-01 12:37:45 +0100

    final

--- src/components/GiphyViewer.js
+++ src/components/GiphyViewer.js
@@ -1,31 +1,22 @@
 import axios from "axios";
-
 import { useState, useEffect } from "react";
-
 import { Button, Card, Row, Dropdown, DropdownButton } from "react-bootstrap";
 
 const GIPHY_URL = `http://api.giphy.com/v1/gifs`;
-
 const API_KEY = `WqCf42eGdmX0mg4iKYuvvMor68YlahOA`;
 
 const GiphyViewer = () => {
   const [gifs, setGifs] = useState([]);
-
   const [term, setTerm] = useState("");
-
   const [limit, setLimit] = useState(15);
 
   useEffect(() => {
     axios
-
       .get(`${GIPHY_URL}/trending?api_key=${API_KEY}`)
-
       .then((response) => {
         console.log(response.data.data);
-
         setGifs(response.data.data);
       })
-
       .catch((error) => {
         console.log(error);
       });
@@ -48,20 +39,15 @@ const GiphyViewer = () => {
   const searchGif = () => {
     if (!term) {
       alert("Please enter a search term");
-
       return;
     }
 
     axios
-
       .get(`${GIPHY_URL}/search?api_key=${API_KEY}&q=${term}&limit=${limit}`)
-
       .then((response) => {
         console.log(response.data.data);
-
         setGifs(response.data.data);
       })
-
       .catch((error) => {
         console.log(error);
       });
@@ -75,15 +61,11 @@ const GiphyViewer = () => {
 
   const getTrending = () => {
     axios
-
       .get(`${GIPHY_URL}/trending?api_key=${API_KEY}&limit=${limit}`)
-
       .then((response) => {
         console.log(response.data.data);
-
         setGifs(response.data.data);
       })
-
       .catch((error) => {
         console.log(error);
       });
@@ -95,15 +77,11 @@ const GiphyViewer = () => {
 
   const getRandom = () => {
     axios
-
       .get(`${GIPHY_URL}/random?api_key=${API_KEY}&limit=${limit}`)
-
       .then((response) => {
         console.log(response.data.data);
-
         setGifs([response.data.data]);
       })
-
       .catch((error) => {
         console.log(error);
       });
@@ -133,19 +111,15 @@ const GiphyViewer = () => {
           onChange={handleChange}
           onKeyUp={handleKeyUp}
         />
-
         <Button variant="primary" onClick={handleClick}>
           Search
         </Button>
-
         <Button variant="info" onClick={handleTrendingClick}>
           Trending
         </Button>
-
         <Button variant="warning" onClick={handleRandomClick}>
           Random
         </Button>
-
         <DropdownButton
           bg="dark"
           text="light"
@@ -160,7 +134,6 @@ const GiphyViewer = () => {
           <Dropdown.Item eventKey={25}>25</Dropdown.Item>
           <Dropdown.Item eventKey={50}>50</Dropdown.Item>
         </DropdownButton>
-
         <Row className="g-4" md={3} xs={1}>
           {gifComponents}
         </Row>
@@ -173,10 +146,9 @@ const GifCard = (props) => {
   return (
     <Card>
       <Card.Img variant="top" src={props.image} />
-
       <Card.Body>
         <Card.Title>
-          <a href={props.url} target="_blank">
+          <a href={props.url} target="_blank" rel="noreferrer">
             {props.title}
           </a>
         </Card.Title>
